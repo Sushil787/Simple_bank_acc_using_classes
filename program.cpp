893eb8e5f9ad960963a5f1f96e724fcb346ca011
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-09-26 09:25:13 +0545

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+class Account{
+    private:
+    int money = 100000;
+    int add;
+    int password;
+    void showMoney(){
+        cout<<money<<"$";
+
+    }
+    void addMoney(){
+        money = money + add;
+        cout<<"Transaction Added with capital of : "<<add<<" $"<<endl;
+        cout<<"Final capital : "<<money<<" $"<<endl;
+    }
+
+    public:
+    void inputAmount(int a){
+        add = a;
+
+    }
+    void showAmount(){
+        addMoney();
+    }
+    void showamount(){
+        showMoney();
+    }
+
+
+
+};
+
+
+//by default password = user1234
+//by default user name = user1234
+//by default user ID = 102D
+int main(){
+    Account acc1;
+    string name;
+    string ID;
+    string password;
+    int userCase;
+    int i;
+    int addmoney;
+
+    cout<<"input user name: "<<endl;
+    cin>>name;
+    cout<<"input user ID: "<<endl;
+    cin>>ID;
+    cout<<"input user Password: "<<endl;
+    cin>>password;
+    cout<<endl;
+    
+    if (name == "user1234" && ID == "102D" && password =="user1234" ){
+        cout<<"Account accessed"<<endl;
+        cout<<"\ndo you want to Add MOney or view money \n press '1' to add money \n press '2' to view account"<<endl;
+        cin>>userCase;
+        switch(userCase){
+            case 1:
+            cout<<"input amount to add"<<endl;
+            cin>>addmoney;
+            acc1.inputAmount(addmoney);
+            cout<<"final amount is :";
+            acc1.showAmount();
+            break;
+            case 2:
+            acc1.showamount();
+            cout<<"\n Thank you for Visiting";
+
+            break;
+            default:
+            cout<<"invalid Entry !! ";
+            break;
+
+
+
+        }
+
+    }
+    else{
+        cout<<"wrong input";
+        cout<<"try Next time";
+       
+    }
+    
+return 0;
+
+
+
+}
